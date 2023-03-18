f2ac235a08f8403266a5aa1cb6e5f47d2b9506dc
VIVEK CHAVAN <vivek27498@gmail.com>
2023-03-18 23:03:17 +0530

    update with openzeplin modifier

--- reEntrancy/Reenter.sol
+++ reEntrancy/Reenter.sol
@@ -1,20 +1,24 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.8;
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Reenter{
+contract Reenter is ReentrancyGuard{
 
     mapping(address=>uint256) balances;
+    // bool lock = false;
 
     function deposit() public payable {
         balances[msg.sender] += msg.value; 
     }
 
-    function withdraw() public{
-
+    function withdraw() public nonReentrant{
+        // require(lock==false,"cannot re enter");
         require(balances[msg.sender]>0,"No amount in senders wallet balance");
+        // lock = true;
         (bool sent,)=(msg.sender).call{value:balances[msg.sender]}("");
         require(sent,"transaction failed");
         balances[msg.sender] = 0;
+        // lock = false;
     }
     
 }
